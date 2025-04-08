37ee4cf4ed3be91a4a9e23be72b67abc2f768ba6
kpapastamatis <34310504+kpapastamatis@users.noreply.github.com>
2025-04-08 11:49:01 +0300

    Update aws-edl-generator.py

--- aws-edl-generator.py
+++ aws-edl-generator.py
@@ -7,9 +7,7 @@ data = response.json()
 # Παίρνουμε μόνο τα IPv4 prefixes
 all_ipv4 = sorted(set([prefix["ip_prefix"] for prefix in data["prefixes"]]))
 
-# Αποθήκευση στο αρχείο
-with open("aws-all-ipv4.txt", "w") as f:
+# Αποθήκευση στο αρχείο edl.txt (ώστε να παρακολουθείται από το GitHub)
+with open("edl.txt", "w") as f:
     for ip in all_ipv4:
         f.write(ip + "\n")
-
-# print(f"✅ Saved {len(all_ipv4)} IPv4 ranges to aws-all-ipv4.txt")
